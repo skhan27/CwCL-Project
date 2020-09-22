483a4d4b312d3a5ea2bff6e48d1e42d3c2eab500
Saim Khan <saim2028@hotmail.com>
2020-09-22 00:47:15 -0400

    basic set up of plane with walls and player character. Player moves using arrow keys

---
+++ Shootosphere/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    private Rigidbody rigidbody;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rigidbody.AddForce(new Vector3(1 * forwardInput * speed, 0, -1 * horizontalInput * speed));
+
+    }
+}
